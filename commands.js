93aa88b6f2f826d115363b0982fc6f15c71b316a
Papa-Cheems <cheemskiller@gmail.com>
2025-10-23 23:13:44 +0800

    Initial commit: Add Discord bot project

--- commands.js
+++ commands.js
@@ -44,6 +44,14 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const DICE_COMMAND = {
+  name: 'dice',
+  description: 'throw a dice and randomly get a number from 1-6',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 2],
+};
+
+const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND, DICE_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
